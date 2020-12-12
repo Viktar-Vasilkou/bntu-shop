bbb7f7928dd1392ebcee4acc7f9cd911d6fe0846
Victor Work <viktar.vasilkou@gmail.com>
2020-12-12 16:23:02 +0300

    Updated hibernate validation to OrderDto

--- src/main/java/by/bntu/fitr/povt/vasilkou/bntu_shop/dto/OrderDto.java
+++ src/main/java/by/bntu/fitr/povt/vasilkou/bntu_shop/dto/OrderDto.java
@@ -6,12 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDto {
+    @Size(min = 10, max = 255, message = "Address should contain more than 10 characters")
     @NotBlank(message = "Address should be not empty")
     public String address;
 }
